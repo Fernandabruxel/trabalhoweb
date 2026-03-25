3ee3b78e1ebc40d63b144ccd79e2d8d7e7b18ad1
Fernandabruxel <nandabruxelbruxel@gmail.com>
2026-03-25 13:39:57 -0300

    Corrige nomes dos métodos no roteador

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const controller = require('../controllers/userController');
 
 // Rotas principais
-router.get('/', controller.home);          
-router.get('/sobre', controller.sobre);     
-router.get('/contato', controller.contato); 
+router.get('/', controller.homePage);          
+router.get('/sobre', controller.aboutPage);     
+router.get('/contato', controller.contactPage); 
 
 // Rotas do CRUD de produtos
-router.get('/produtos', controller.produtos);                    // listar produtos
-router.post('/produtos/adicionar', controller.adicionar);        // add
-router.get('/produtos/excluir/:id', controller.excluir);         // excluir
-router.get('/produtos/editar/:id', controller.editarForm);       // form de edição
-router.post('/produtos/editar/:id', controller.editar);          // salvar edição
+router.get('/produtos', controller.itemsList);                    // listar produtos
+router.post('/produtos/adicionar', controller.addItem);        // add
+router.get('/produtos/excluir/:id', controller.deleteItem);         // excluir
+router.get('/produtos/editar/:id', controller.loadEditForm);       // form de edição
+router.post('/produtos/editar/:id', controller.saveEdit);          // salvar edição
 
 module.exports = router;
