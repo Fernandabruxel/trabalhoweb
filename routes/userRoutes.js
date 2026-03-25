ea5c3d8acdf70d1e9a180b29ef3afcf302c788ab
Fernandabruxel <nandabruxelbruxel@gmail.com>
2026-03-25 19:16:58 -0300

    userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,6 +8,8 @@ router.get('/sobre', controller.aboutPage);
 router.get('/contato', controller.contactPage); 
 
 // Rotas do CRUD de produtos
+// router.get = define uma rota que responde a requisiçoes do tipo GET
+// post = enviando dados
 router.get('/produtos', controller.itemsList);                    // listar produtos
 router.post('/produtos/adicionar', controller.addItem);        // add
 router.get('/produtos/excluir/:id', controller.deleteItem);         // excluir
