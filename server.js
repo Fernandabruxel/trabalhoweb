7ad1810c218e38171cf42e1daf2227de145783d5
Fernandabruxel <nandabruxelbruxel@gmail.com>
2026-03-25 19:19:58 -0300

    server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const bodyParser = require("body-parser");
 
 // config do motor de templates
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); //caminho para a pasta 'views'
 
-// config dos middlewares
+// config dos middlewares = função que executa antes de chegar nas rotas
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', userRoutes);
